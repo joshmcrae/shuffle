4e34a64b2e8337f1f45837774d35d7b30626ce48
Joshua McRae <josh@Joshuas-Mini.lan>
2022-12-29 15:06:23 +1000

    Added tab bar

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,20 +1,27 @@
 <template>
-  <component :is="screen"/>
+    <div class="w-full h-full flex flex-col">
+        <component
+            class="flex-grow"
+            :is="screen"/>
+        <TabBar class="basis-14 grow-0 shrink-0"/>
+    </div>
 </template>
 
 <script>
 import { mapState } from 'pinia'
 import { useNavStore } from '../store/nav'
+import TabBar from './TabBar.vue'
 import Meals from './Meals.vue'
 import Meal from './Meal.vue'
 import Shuffle from './Shuffle.vue'
 
 export default {
-  components: {
-    Meals,
-    Meal,
-    Shuffle
-  },
-  computed: mapState(useNavStore, ['screen'])
+    components: {
+        TabBar,
+        Meals,
+        Meal,
+        Shuffle
+    },
+    computed: mapState(useNavStore, ['screen'])
 }
 </script>
---
+++ src/components/TabBar.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="w-full flex justify-between bg-gray-50">
+        <div
+            v-for="tab in tabs"
+            class="p-6 cursor-pointer">
+            <component :is="tab.icon"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import { PhGearSix, PhListPlus, PhShuffle, PhClockCounterClockwise } from 'phosphor-vue'
+
+export default {
+    name: 'TabBar',
+    components: {
+        PhGearSix
+    },
+    computed: {
+        tabs() {
+            return [
+                {
+                    icon: PhListPlus
+                },
+                {
+                    icon: PhShuffle
+                },
+                {
+                    icon: PhClockCounterClockwise
+                },
+                {
+                    icon: PhGearSix
+                }
+            ]
+        }
+    }
+}
+</script>
