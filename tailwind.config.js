03d795b4332a92c812d7207586dbcf74c2b5c43e
Joshua McRae <josh@Joshuas-Mini.lan>
2023-01-09 15:55:34 +1000

    Another one

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-      './src/components/**/*.vue'
+      './src/components/**/*.vue',
+      './src/index.htm'
   ],
   theme: {
     extend: {
